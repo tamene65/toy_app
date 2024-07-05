0ed0e001fdeb3e97f51ebbdb1bf51a109bb0833b
Ubuntu <ubuntu@ip-172-31-28-246.eu-north-1.compute.internal>
2024-07-05 13:55:41 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+   # Allow connections to local server on cloud IDE.
+  config.hosts.clear
+
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
